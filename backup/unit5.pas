3633cf4eff38f68268b6629c0a6e4f84f1df6fee
riyantama22 <tamariyan54@gmail.com>
2024-01-11 11:45:02 +0700

    lazarus

---
+++ backup/unit5.pas
@@ -0,0 +1,60 @@
+unit Unit5;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, DB, SQLDB, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  DBCtrls, DBGrids, ZConnection, ZDataset, LR_Class, LR_DBSet;
+
+type
+
+  { TForm5 }
+
+  TForm5 = class(TForm)
+    Button1: TButton;
+    DataSource1: TDataSource;
+    DataSource2: TDataSource;
+    DBGrid1: TDBGrid;
+    DBGrid2: TDBGrid;
+    DBNavigator1: TDBNavigator;
+    frDBDataSet1: TfrDBDataSet;
+    frReport1: TfrReport;
+    Label1: TLabel;
+    ZConnection1: TZConnection;
+    ZConnection2: TZConnection;
+    ZQuery1: TZQuery;
+    ZQuery2: TZQuery;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm5 }
+
+procedure TForm5.Button1Click(Sender: TObject);
+var
+  s:string;
+
+begin
+  s:=extractfilepath(application.ExeName)+'Untitled.lrf';
+  frDBDataset1.DataSet:=ZQuery1;
+  frReport1.Dataset:=frDBDataset1;
+  frReport1.LoadFromFile(s);
+  frReport1.ShowReport;
+
+
+end;
+
+end.
+
